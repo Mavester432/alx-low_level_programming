f03bded3712663f6361443a80c61591f87099491
Mavester432 <mavester432@gmail.com>
2023-09-21 00:03:06 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,27 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int bytes_written = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 	return (-1);
 
-if (text_content != NULL)
-{
-	while (text_content[bytes_written] != '\0')
-	bytes_written++;
-
-if (write(open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600), text_content, bytes_written) == -1)
-{
-return (-1);
-}
-}
-else
-{
-	if (open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600) == -1)
+	if (text_content != NULL)
 	{
-	return (-1);
+	for (len = 0; text_content[len];)
+	len++;
 	}
-}
-return (1);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	return (-1);
+
+	close(o);
+
+	return (1);
 }
